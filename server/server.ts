2c122a992585d02e737560a39e7ffb6d8f0e2c53
Yash Velagapudi <yashv6655@gmail.com>
2021-01-06 19:06:18 -0800

    Installed morgan middleware

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,30 @@
+require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 const port = process.env.PORT || "4000";
+
+app.use(morgan("tiny"));
+
+// fetching all restaurants
+app.get("/restaurants/api/get-all", (req, res) => {
+  res.status(200);
+  res.json({
+    data: {
+      restaurants: "all the restaurants",
+    },
+  });
+});
+
+app.get("/restaurants/api/get-single-restaurant/:id", (req, res) => {
+  console.log(res);
+});
+
+app.post("/restaurants/api/create-restaurant", (req, res) => {
+  console.log(req);
+});
 
 app.listen(port, (err) => {
   if (err) console.log(err);
