cdd2fe5bda244390826c3068972662e602a94379
Yash Velagapudi <yashv6655@gmail.com>
2021-01-07 17:39:27 -0800

    Removed env variables

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,12 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-});
+const pool = new Pool();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
