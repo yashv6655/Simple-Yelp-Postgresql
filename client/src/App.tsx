bee4bf9de3d50a091e0f03de11d39ac55a9ecce1
Yash Velagapudi <yashv6655@gmail.com>
2021-01-09 17:35:16 -0800

    Context provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { RestaurantContextProvider } from "./context/RestaurantContext";
 import Home from "./routes/Home";
 import RestaurantDetails from "./routes/RestaurantDetails";
 import UpdatePage from "./routes/UpdatePage";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/restaurants/:id/update">
-          <UpdatePage />
-        </Route>
-        <Route exact path="/details/:id">
-          <RestaurantDetails />
-        </Route>
-      </Switch>
-    </Router>
+    <RestaurantContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/restaurants/:id/update">
+              <UpdatePage />
+            </Route>
+            <Route exact path="/details/:id">
+              <RestaurantDetails />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantContextProvider>
   );
 }
---
+++ client/src/context/RestaurantContext.tsx
@@ -0,0 +1,12 @@
+import React, { useState, createContext } from "react";
+
+export const RestaurantContext = createContext({});
+
+export const RestaurantContextProvider = ({ children }) => {
+  const [restaurants, setRestaurants] = useState([]);
+  return (
+    <RestaurantContext.Provider value={{ restaurants, setRestaurants }}>
+      {children}
+    </RestaurantContext.Provider>
+  );
+};
